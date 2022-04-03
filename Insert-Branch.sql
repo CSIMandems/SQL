8ea8f15eafacedeb9226ea611c8d9ba2756adf3d
Shaan Bhoi <46392837+ShaanBhoi@users.noreply.github.com>
2022-04-03 18:26:36 -0400

    Create Insert-Branch.sql

---
+++ Insert-Branch.sql
@@ -0,0 +1,16 @@
+INSERT INTO Branch VALUES
+(001,486,'Montreal Rd','Ottawa','Ontario','D9Y2W6'),
+(002,54,'Saint Laurent Blvd','Ottawa','Ontario','K3F9T6'),
+(003,984,'Rideau St','Ottawa','Ontario','E9U3Q5'),
+
+(004,18,'Anyplace Drive','Montreal','Quebec','A6U8V5'),
+(005,9875,'Nowhere Lane','Montreal','Quebec','D5H2V1'),
+(006,658,'Overthere Way','Montreal','Quebec','S2R9N5'),
+
+(007,352,'Donald St','Toronto','Ontario','A6U8V5'),
+(008,5,'Aviation Parkway','Toronto','Ontario','D5H2V1'),
+(009,45823,'Thorncliffe','Toronto','Ontario','S2R9N5');
+
+
+SELECT * FROM Branch;
+--DELETE FROM Branch *;
