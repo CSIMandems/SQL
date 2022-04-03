7d6ac031ac464a9a5695882ed272ff4cfa5fec9a
Shaan Bhoi <46392837+ShaanBhoi@users.noreply.github.com>
2022-04-03 19:34:42 -0400

    Create Insert-Employee.sql

---
+++ Insert-Employee.sql
@@ -0,0 +1,5 @@
+INSERT INTO employee VALUES
+(1,'Dentist','Lily',NULL,'Ingram','1966-01-01','F',570271493,12,'Rosemary Lane','Ottawa','Ontario','D9Y6D5',6133317753,'Lily.Ingram@dentist.com',349,286214123141,'$60.00',NULL,1,1),
+(2,'Manager','Deon','Michael','Phillips','1987-08-24','M',823878518,56,'Lavender Drive','Ottawa','Ontario','D9Y5F2',6132611739,'Deon.M.Phillips@manager.com',584,651164639864,'$40.00',NULL,1,2),
+(3,'Receptionist','Kevin',NULL,'Mcdaniel','1986-07-21','M',265318549,'57 Unit 6','Thyme Avenue','Ottawa','Ontario','D9Y5D9','6137669055','Kevin.Mcdaniel@Receptionist.com',283,279025691488,'$30.00',2,1,3);
+SELECT * FROM employee;
