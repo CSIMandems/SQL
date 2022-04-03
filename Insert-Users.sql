acc981ea4899ae21bf0f5569f844f43e58e1938f
Shaan Bhoi <46392837+ShaanBhoi@users.noreply.github.com>
2022-04-03 18:36:06 -0400

    Create Insert-Users.sql

---
+++ Insert-Users.sql
@@ -0,0 +1,20 @@
+INSERT INTO Users VALUES 
+(1,'Lily Ingram','password'),
+(2,'Deon Phillips','password'),
+(3,'Kevin Mcdaniel','password'),
+(4,'Lindsay Benson','password'),
+(5,'Felicity Booker','password'),
+(6,'Blaine Mcgee','password'),
+(7,'Emery Richards','password'),
+(8,'Kasen Solis','password'),
+(9,'Diana Gilmore','password'),
+(10,'Jayden Haney','password'),
+(11,'Nylah Mcclain','password'),
+(12,'Jackson Cobb','password'),
+(13,'Rylie Hess','password'),
+(14,'Aubrey Wells','password'),
+(15,'Ramiro Duncan','password'),
+(16,'Reagan Preston','password');
+
+SELECT * FROM Users;
+--DELETE * FROM Users
