7ee659879c9b48babb4f7ef4dc6b543166d98d7a
Shaan Bhoi <46392837+ShaanBhoi@users.noreply.github.com>
2022-04-04 19:07:30 -0400

    Update Insert-Users.sql

--- Insert-Users.sql
+++ Insert-Users.sql
@@ -1,20 +1,107 @@
+
+SELECT * FROM users ; 
+
 INSERT INTO Users VALUES 
+-- 		EMPLOYEES		--
+-- BRANCH  1
 (1,'Lily Ingram','password'),
 (2,'Deon Phillips','password'),
 (3,'Kevin Mcdaniel','password'),
 (4,'Lindsay Benson','password'),
+
+-- BRANCH  4
 (5,'Felicity Booker','password'),
 (6,'Blaine Mcgee','password'),
 (7,'Emery Richards','password'),
 (8,'Kasen Solis','password'),
+
+-- BRANCH  7
 (9,'Diana Gilmore','password'),
 (10,'Jayden Haney','password'),
 (11,'Nylah Mcclain','password'),
 (12,'Jackson Cobb','password'),
+
+-- BRANCH  2
 (13,'Rylie Hess','password'),
 (14,'Aubrey Wells','password'),
 (15,'Ramiro Duncan','password'),
-(16,'Reagan Preston','password');
+(16,'Reagan Preston','password'),
+
+-- BRANCH  3
+(17,'Madyson Hull','password'),
+(18,'Gabrielle Ramirez','password'),
+(19,'Markus Hodges','password'),
+(20,'Ryker Sanchez','password'),
+
+-- BRANCH  5
+(21,'Zander Ponce','password'),
+(22,'Armani Lozano','password'),
+(23,'Jaelynn Wolf','password'),
+(24,'Kendal Ali','password'),
+
+-- BRANCH  6
+(25,'Avery Figueroa','password'),
+(26,'Carson Spence','password'),
+(27,'Andrew Russell','password'),
+(28,'Esteban Dillon','password'),
+-- BRANCH  8
+(29,'Karley Le','password'),
+(30,'Jadiel Parrish','password'),
+(31,'Laura Salazar','password'),
+(32,'Emilie Knapp','password'),
+-- BRANCH  9
+(33,'Dylan Barrera','password'),
+(34,'Braiden Ward','password'),
+(35,'Quinn Montes','password'),
+
+
+--		Patients	--
+-- BRANCH  1
+(36,'Roy Colon','password'),
+(37,'Jaelyn Owens','password'),
+(38,'Clara Wu','password'),
+
+-- BRANCH  2
+(39,'Octavio Dudley','password'),
+(40,'Melina Yu','password'),
+(41,'Marlee Roman','password'),
+
+-- BRANCH  3
+(42,'Thomas Stevens','password'),
+(43,'Lilianna Perez','password'),
+(44,'Elisabeth Dalton','password'),
+
+-- BRANCH  4
+(45,'Diego Burnett','password'),
+-- BRANCH  5
+(46,'Diego Hernandez','password'),
+-- BRANCH  6
+(47,'Kingston Foley','password'),
+-- BRANCH  7
+(48,'Catalina Bray','password'),
+-- BRANCH  8
+(49,'Zachery Miles','password'),
+-- BRANCH  9
+(50,'Laurel Jennings','password'),
+
+
+-- ALL FAMILIES IN BRANCH 1
+--FAMILY#1 regular family, all same address
+(51,'Evan Harrington','password'),
+(52,'Cheyanne Harrington','password'),
+(53,'Malcolm Harrington','password'),
+(54,'Alison Harrington','password'),
+
+
+--FAMILY #2 regular family, all same address
+(55,'Katelyn Terry','password'),
+(56,'Tyler Terry','password'),
+(57,'Kale Terry','password'),
+
+
+--FAMILY #3 kid is a patient but parent is responsible party
+(58,'Roberto Hoffman','password'),
+(59,'Briley Hoffman','password');
 
 SELECT * FROM Users;
---DELETE * FROM Users
+-- --DELETE * FROM Users
