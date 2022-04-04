e341dd6c8676f0ff3712f1e1ceff6d4e71eceb0c
Shaan Bhoi <46392837+ShaanBhoi@users.noreply.github.com>
2022-04-04 18:41:32 -0400

    Update Insert-Employee.sql

--- Insert-Employee.sql
+++ Insert-Employee.sql
@@ -1,5 +1,16 @@
 INSERT INTO employee VALUES
+-- BRANCH DETAILS -- (001,486,'Montreal Rd','Ottawa','Ontario','D9Y2W6') 
 (1,'Dentist','Lily',NULL,'Ingram','1966-01-01','F',570271493,12,'Rosemary Lane','Ottawa','Ontario','D9Y6D5',6133317753,'Lily.Ingram@dentist.com',349,286214123141,'$60.00',NULL,1,1),
 (2,'Manager','Deon','Michael','Phillips','1987-08-24','M',823878518,56,'Lavender Drive','Ottawa','Ontario','D9Y5F2',6132611739,'Deon.M.Phillips@manager.com',584,651164639864,'$40.00',NULL,1,2),
-(3,'Receptionist','Kevin',NULL,'Mcdaniel','1986-07-21','M',265318549,'57 Unit 6','Thyme Avenue','Ottawa','Ontario','D9Y5D9','6137669055','Kevin.Mcdaniel@Receptionist.com',283,279025691488,'$30.00',2,1,3);
+(3,'Receptionist','Kevin',NULL,'Mcdaniel','1986-07-21','M',265318549,'57 Unit 6','Thyme Avenue','Ottawa','Ontario','D9Y5D9','6137669055','Kevin.Mcdaniel@Receptionist.com',283,279025691488,'$30.00',2,1,3),
+(4,'Hygenist','Lindsay',NULL,'Benson','1983-04-14','F',237575328,78,'Coriander Circle','Ottawa','Ontario','D9Y2R3','6135733638','Lindsay.Benson@hygenist.com',821,363386740842,'$30.00',2,1,4),
+
+-- BRANCH DETAILS -- (004,18,'Anyplace Drive','Montreal','Quebec','A6U8V5'),
+(5,'Dentist',		'Felicity',NULL,'Booker',	'1968-01-05','F',644818688,1410,	'Rose Ave',			'Montreal','Quebec','A6U9D2',	5141928801,		'Felicity.Booker@dentist.com',			513,511665382754,'$60.00',NULL,	4,5),
+(6,'Manager',		'Blaine',NULL,'Mcgee',	'1977-04-14','M',751428448,589,		'Lilac Lane',		'Montreal','Quebec','A6U3H5',	5141078812,		'Blaine.Mcgee@manager.com',			976,325373177026,'$40.00',NULL,	4,6),
+(7,'Receptionist','Emery',NULL,'Richards',	'1983-06-16','M',846533586,'648-B89',	'Lily Circle',	'Montreal','Quebec','A6UQ2U',	5149684458,		'Emery.Richards@receptionist.com',	168,942059846868,'$30.00',6,	4,7),
+(8,'Hygenist',	'Kasen',NULL,'Solis',	'1993-07-21','M',809823770,183,		'Orchid Way',		'Montreal','Quebec','A6UH9K',	5142439780,		'Kasen.Solis@hygenist.com',		258,697895399617,'$30.00',6,	4,8);
+
+
 SELECT * FROM employee;
+--DELETE * FROM employee
