6ec4695006031267549418044b409539de52e545
Shaan Bhoi <46392837+ShaanBhoi@users.noreply.github.com>
2022-04-04 22:55:05 -0400

    Update Insert-Patient.sql

--- Insert-Patient.sql
+++ Insert-Patient.sql
@@ -2,7 +2,7 @@ INSERT INTO patient VALUES
 
 (1,'Roy',NULL,'Colon',		'1965-05-17','M',683401662,90489026346815766588,56	,'Hotsauce Drive',	'Ottawa','Ontario','D9Y6D5',6133724404,'Roy.Colon@patient.com',36,NULL),
 (2,'Jaelyn',NULL,'Owens',	'1986-06-25','M',797638725,86897145633155104943,235	,'Ghostpepper Road','Ottawa','Ontario','D9Y7J6',6130365226,'Jaelyn.Owens@patient.com',37,NULL),
-(3,'Clara',NULL,'Wu',		'1978-02-20','F',487131841,98370934436219627481,7861,'Chili Circle',	'Ottawa','Ontario','D9Y2E9',6133298619,'Clara.Wu@patient.com',38,NULL);
+(3,'Clara',NULL,'Wu',		'1978-02-20','F',487131841,98370934436219627481,7861,'Chili Circle',	'Ottawa','Ontario','D9Y2E9',6133298619,'Clara.Wu@patient.com',38,NULL),
 
 
 
@@ -29,6 +29,15 @@ INSERT INTO patient VALUES
 (9,'Tyler',NULL,'Terry'	,			'2004-01-13','M',	637701900,87161421096627391973,34,'Not Stepmother Way'	,'Ottawa','Ontario','D9Y6R3',6135370356,'Tyler.Terry@patient.com'		,56,2),
 (10,'Kale',NULL,'Terry'	,			'2007-02-21','M',	601247645,87161421096627391973,34,'Not Stepmother Way'	,'Ottawa','Ontario','D9Y6R3',6135370356,'Kale.Terry@patient.com'		,57,2),
 
+
+
+-- --FAMILY #3 kid is a patient but parent is responsible party and is not a patient
+-- (58,'Roberto Hoffman')<-RP for kid, is not a patient // done
+-- RP : --(3,'Roberto',NULL,'Hoffman'			,'1968-03-23','M',169167110,59256175755916609149,596,'Father Way'		,'Ottawa','Ontario','D9Y5T8',6137941753,'Roberto.Hoffman@responsibleparty.com'		,58);
+-- (59,'Briley Hoffman')
+
+
+(11,'Briley','Dustan','Hoffman'			,'2001-09-11','M',585006823,59256175755916609149,596,'Father Way'		,'Ottawa','Ontario','D9Y5T8',6137941753,'Briley.D.Hoffman@patient.com'		,59,3);
 
 
 SELECT * FROM patient;
