4f925e397b073e425ef9b69f5f7b435c6df4bc1d
Shaan Bhoi <46392837+ShaanBhoi@users.noreply.github.com>
2022-04-04 20:51:56 -0400

    Create Insert-Patient.sql

---
+++ Insert-Patient.sql
@@ -0,0 +1,10 @@
+INSERT INTO patient VALUES
+
+(1,'Roy',NULL,'Colon',		'1965-05-17','M',683401662,90489026346815766588,56	,'Hotsauce Drive',	'Ottawa','Ontario','D9Y6D5',6133724404,'Roy.Colon@patient.com',36,NULL),
+(2,'Jaelyn',NULL,'Owens',	'1986-06-25','M',797638725,86897145633155104943,235	,'Ghostpepper Road','Ottawa','Ontario','D9Y7J6',6130365226,'Jaelyn.Owens@patient.com',37,NULL),
+(3,'Clara',NULL,'Wu',		'1978-02-20','F',487131841,98370934436219627481,7861,'Chili Circle',	'Ottawa','Ontario','D9Y2E9',6133298619,'Clara.Wu@patient.com',38,NULL);
+
+
+
+SELECT * FROM patient;
+-- -- DELETE * FROM patient;
