3f60bcf5cafcea86d40ea9d992836b69863661f0
Shaan Bhoi <46392837+ShaanBhoi@users.noreply.github.com>
2022-04-04 21:14:34 -0400

    Create Insert-responsibleparty.sql

---
+++ Insert-responsibleparty.sql
@@ -0,0 +1,8 @@
+INSERT INTO responsible_party VALUES
+
+---- (52,'Cheyanne Harrington')<-RP for kids -- insert into rp first
+---- (55,'Katelyn Terry')<-RP for kids -- insert into RP first
+(1,'Cheyanne',NULL,'Harrington'	,'1990-06-14','F',425844905,31242080627392556735,26,'Mother Lane'		,'Ottawa','Ontario','D9Y3H4',6132413174,'Cheyanne.Harrington@responsibleparty.com'	,52),
+(2,'Katelyn','Brooke','Terry'	,'1991-12-18','F',790084336,87161421096627391973,469,'Stepmother Way'	,'Ottawa','Ontario','D9Y2R9',6135809128,'Katelyn.Terry@responsibleparty.com'		,55);
+
+SELECT * FROM responsible_party;
