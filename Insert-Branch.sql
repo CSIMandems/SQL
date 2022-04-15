3009d90d12b87277e63156faf3b4534fc95339b4
Lilydahamna <liliadahamna2000@gmail.com>
2022-04-15 16:15:28 -0400

    updated insert

--- Insert-Branch.sql
+++ Insert-Branch.sql
@@ -1,15 +1,17 @@
-INSERT INTO Branch VALUES
-(001,486,'Montreal Rd','Ottawa','Ontario','D9Y2W6'),
-(002,54,'Saint Laurent Blvd','Ottawa','Ontario','K3F9T6'),
-(003,984,'Rideau St','Ottawa','Ontario','E9U3Q5'),
+INSERT INTO Branch (civic_number, street_name, city, province, postal_code)
 
-(004,18,'Anyplace Drive','Montreal','Quebec','A6U8V5'),
-(005,9875,'Nowhere Lane','Montreal','Quebec','D5H2V1'),
-(006,658,'Overthere Way','Montreal','Quebec','S2R9N5'),
+VALUES
+(486,'Montreal Rd','Ottawa','Ontario','D9Y2W6'),
+(54,'Saint Laurent Blvd','Ottawa','Ontario','K3F9T6'),
+(984,'Rideau St','Ottawa','Ontario','E9U3Q5'),
 
-(007,352,'Donald St','Toronto','Ontario','A6U8V5'),
-(008,5,'Aviation Parkway','Toronto','Ontario','D5H2V1'),
-(009,45823,'Thorncliffe','Toronto','Ontario','S2R9N5');
+(18,'Anyplace Drive','Montreal','Quebec','A6U8V5'),
+(9875,'Nowhere Lane','Montreal','Quebec','D5H2V1'),
+(658,'Overthere Way','Montreal','Quebec','S2R9N5'),
+
+(352,'Donald St','Toronto','Ontario','A6U8V5'),
+(5,'Aviation Parkway','Toronto','Ontario','D5H2V1'),
+(45823,'Thorncliffe','Toronto','Ontario','S2R9N5');
 
 
 SELECT * FROM Branch;
