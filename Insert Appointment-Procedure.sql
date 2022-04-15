33f33ad7e5140ff394cca024d2b16331ba5bc4ed
Shaan Bhoi <46392837+ShaanBhoi@users.noreply.github.com>
2022-04-15 02:59:06 -0400

    Create Insert Appointment-Procedure.sql

---
+++ Insert Appointment-Procedure.sql
@@ -0,0 +1,22 @@
+INSERT INTO appointment_procedure VALUES 
+(1,'2022-04-04','All Teeth','1x Dental Mirror, 2x Dental Scalars, 1x Flouride Polish, 1x Polish Brush Head, 2x Water Flosser',80.00,1,1,1,1),
+(2,'2022-08-04','All Teeth','1x Dental Mirror, 2x Dental Scalars, 1x Flouride Polish, 1x Polish Brush Head, 2x Water Flosser',80.00,1,1,2,2),
+(3,'2022-12-04','All Teeth','1x Dental Mirror, 2x Dental Scalars, 1x Flouride Polish, 1x Polish Brush Head, 2x Water Flosser',80.00,1,1,3,3),
+
+(4,'2022-04-04','All Teeth','1x Dental Mirror, 2x Dental Scalars, 1x Flouride Polish, 1x Polish Brush Head, 2x Water Flosser',80.00,1,2,4,4),
+(5,'2022-08-04','All Teeth','1x Dental Mirror, 2x Dental Scalars, 1x Flouride Polish, 1x Polish Brush Head, 2x Water Flosser',80.00,1,2,5,5),
+(6,'2022-12-04','All Teeth','1x Dental Mirror, 2x Dental Scalars, 1x Flouride Polish, 1x Polish Brush Head, 2x Water Flosser',80.00,1,2,6,6),
+
+(7,'2022-04-04','All Teeth','1x Dental Mirror, 2x Dental Scalars, 1x Flouride Polish, 1x Polish Brush Head, 2x Water Flosser',80.00,1,3,7,7),
+(8,'2022-08-04','All Teeth','1x Dental Mirror, 2x Dental Scalars, 1x Flouride Polish, 1x Polish Brush Head, 2x Water Flosser',80.00,1,3,8,8),
+(9,'2022-12-04','All Teeth','1x Dental Mirror, 2x Dental Scalars, 1x Flouride Polish, 1x Polish Brush Head, 2x Water Flosser',80.00,1,3,9,9),
+
+(10,'2022-04-04','All Teeth','1x Dental Mirror, 2x Dental Scalars, 1x Flouride Polish, 1x Polish Brush Head, 2x Water Flosser',80.00,1,4,10,10),
+(11,'2022-04-04','All Teeth','1x Dental Mirror, 2x Dental Scalars, 1x Flouride Polish, 1x Polish Brush Head, 2x Water Flosser',80.00,1,5,11,11),
+
+(12,'2022-04-04','All Teeth','2x Mouth Guard, 1x Lead Vest, 2x Press Mould, 2x Dry Rinse',200.00,8,6,12,12),
+(13,'2022-04-04','All Teeth','2x Mouth Guard, 1x Lead Vest, 2x Press Mould, 2x Dry Rinse',200.00,8,7,13,13),
+
+(14,'2022-04-04','All Teeth','2x Power Chain, 2x Coil, 6x K-Hook, 4x Seperator, 32x Bracket, 8x Bracket Hook',3000.00,9,6,14,14),
+(15,'2022-04-04','All Teeth','2x Power Chain, 2x Coil, 6x K-Hook, 4x Seperator, 32x Bracket, 8x Bracket Hook',3000.00,9,7,15,15);
+select * from appointment_procedure;
