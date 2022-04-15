3fc78263601948bf81ec8ebed48f4c5dfa6a80e0
Lilydahamna <liliadahamna2000@gmail.com>
2022-04-15 16:34:17 -0400

    updated insert

--- Insert-Users.sql
+++ Insert-Users.sql
@@ -1,107 +1,109 @@
 
-SELECT * FROM users ; 
 
-INSERT INTO Users VALUES 
+
+INSERT INTO Users (username, password)
+
+VALUES 
 -- 		EMPLOYEES		--
 -- BRANCH  1
-(1,'Lily Ingram','password'),
-(2,'Deon Phillips','password'),
-(3,'Kevin Mcdaniel','password'),
-(4,'Lindsay Benson','password'),
+('Lily Ingram','password'),
+('Deon Phillips','password'),
+('Kevin Mcdaniel','password'),
+('Lindsay Benson','password'),
 
 -- BRANCH  4
-(5,'Felicity Booker','password'),
-(6,'Blaine Mcgee','password'),
-(7,'Emery Richards','password'),
-(8,'Kasen Solis','password'),
+('Felicity Booker','password'),
+('Blaine Mcgee','password'),
+('Emery Richards','password'),
+('Kasen Solis','password'),
 
 -- BRANCH  7
-(9,'Diana Gilmore','password'),
-(10,'Jayden Haney','password'),
-(11,'Nylah Mcclain','password'),
-(12,'Jackson Cobb','password'),
+('Diana Gilmore','password'),
+('Jayden Haney','password'),
+('Nylah Mcclain','password'),
+('Jackson Cobb','password'),
 
 -- BRANCH  2
-(13,'Rylie Hess','password'),
-(14,'Aubrey Wells','password'),
-(15,'Ramiro Duncan','password'),
-(16,'Reagan Preston','password'),
+('Rylie Hess','password'),
+('Aubrey Wells','password'),
+('Ramiro Duncan','password'),
+('Reagan Preston','password'),
 
 -- BRANCH  3
-(17,'Madyson Hull','password'),
-(18,'Gabrielle Ramirez','password'),
-(19,'Markus Hodges','password'),
-(20,'Ryker Sanchez','password'),
+('Madyson Hull','password'),
+('Gabrielle Ramirez','password'),
+('Markus Hodges','password'),
+('Ryker Sanchez','password'),
 
 -- BRANCH  5
-(21,'Zander Ponce','password'),
-(22,'Armani Lozano','password'),
-(23,'Jaelynn Wolf','password'),
-(24,'Kendal Ali','password'),
+('Zander Ponce','password'),
+('Armani Lozano','password'),
+('Jaelynn Wolf','password'),
+('Kendal Ali','password'),
 
 -- BRANCH  6
-(25,'Avery Figueroa','password'),
-(26,'Carson Spence','password'),
-(27,'Andrew Russell','password'),
-(28,'Esteban Dillon','password'),
+('Avery Figueroa','password'),
+('Carson Spence','password'),
+('Andrew Russell','password'),
+('Esteban Dillon','password'),
 -- BRANCH  8
-(29,'Karley Le','password'),
-(30,'Jadiel Parrish','password'),
-(31,'Laura Salazar','password'),
-(32,'Emilie Knapp','password'),
+('Karley Le','password'),
+('Jadiel Parrish','password'),
+('Laura Salazar','password'),
+('Emilie Knapp','password'),
 -- BRANCH  9
-(33,'Dylan Barrera','password'),
-(34,'Braiden Ward','password'),
-(35,'Quinn Montes','password'),
+('Dylan Barrera','password'),
+('Braiden Ward','password'),
+('Quinn Montes','password'),
 
 
 --		Patients	--
 -- BRANCH  1
-(36,'Roy Colon','password'),
-(37,'Jaelyn Owens','password'),
-(38,'Clara Wu','password'),
+('Roy Colon','password'),
+('Jaelyn Owens','password'),
+('Clara Wu','password'),
 
 -- BRANCH  4
-(39,'Octavio Dudley','password'),
-(40,'Melina Yu','password'),
-(41,'Marlee Roman','password'),
+('Octavio Dudley','password'),
+('Melina Yu','password'),
+('Marlee Roman','password'),
 
 -- BRANCH  7
-(42,'Thomas Stevens','password'),
-(43,'Lilianna Perez','password'),
-(44,'Elisabeth Dalton','password'),
+('Thomas Stevens','password'),
+('Lilianna Perez','password'),
+('Elisabeth Dalton','password'),
 
 -- BRANCH  2
-(45,'Diego Burnett','password'),
+('Diego Burnett','password'),
 -- BRANCH  5
-(46,'Diego Hernandez','password'),
+('Diego Hernandez','password'),
 -- BRANCH  6
-(47,'Kingston Foley','password'),
+('Kingston Foley','password'),
 -- BRANCH  3
-(48,'Catalina Bray','password'),
+('Catalina Bray','password'),
 -- BRANCH  8
-(49,'Zachery Miles','password'),
+('Zachery Miles','password'),
 -- BRANCH  9
-(50,'Laurel Jennings','password'),
+('Laurel Jennings','password'),
 
 
 -- ALL FAMILIES IN BRANCH 1
 --FAMILY#1 regular family, all same address all patients
-(51,'Evan Harrington','password'),
-(52,'Cheyanne Harrington','password'),
-(53,'Malcolm Harrington','password'),
-(54,'Alison Harrington','password'),
+('Evan Harrington','password'),
+('Cheyanne Harrington','password'),
+('Malcolm Harrington','password'),
+('Alison Harrington','password'),
 
 
 --FAMILY #2 regular family, all same address, all patients
-(55,'Katelyn Terry','password'),
-(56,'Tyler Terry','password'),
-(57,'Kale Terry','password'),
+('Katelyn Terry','password'),
+('Tyler Terry','password'),
+('Kale Terry','password'),
 
 
 --FAMILY #3 kid is a patient but parent is responsible party
-(58,'Roberto Hoffman','password'),
-(59,'Briley Hoffman','password');
+('Roberto Hoffman','password'),
+('Briley Hoffman','password');
 
 SELECT * FROM Users;
 
