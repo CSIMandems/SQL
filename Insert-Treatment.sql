184151a4926adae87b75d719c4528cc55d7b1772
Shaan Bhoi <46392837+ShaanBhoi@users.noreply.github.com>
2022-04-15 18:52:42 -0400

    Update Insert-Treatment.sql

--- Insert-Treatment.sql
+++ Insert-Treatment.sql
@@ -1,4 +1,6 @@
-INSERT INTO treatment VALUES
-(1,'Malocclusion Moderate','Tooth aches, overbite and misaligned jaw','TOP : Primary incisors & canines, Bottom : Secondary molars','Braces','n/a','n/a',14),
-(2,'Malocclusion Severe','Overbite and jaw pain','TOP : Primary incisors, canines, pre-molars and molars','Braces','n/a','n/a',15);
+INSERT INTO treatment 
+(  diagnosis,     symptoms ,    teeth ,     treatment ,     medications ,     comments ,     appointment_id)
+VALUES
+('Malocclusion Moderate','Tooth aches, overbite and misaligned jaw','TOP : Primary incisors & canines, Bottom : Secondary molars','Braces','n/a','n/a',14),
+('Malocclusion Severe','Overbite and jaw pain','TOP : Primary incisors, canines, pre-molars and molars','Braces','n/a','n/a',15);
 SELECT * FROM treatment;
